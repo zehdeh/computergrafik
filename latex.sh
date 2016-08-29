d755f929398721fb7d2c0cdd15f7a8fdaef2b5d3
Fabian Fritz <fabianfuture@web.de>
2016-08-29 14:35:42 +0200

    Expand introduction. Make references work

--- latex.sh
+++ latex.sh
@@ -3,6 +3,9 @@ current_dir="$(pwd)"
 filename=$(basename "$1")
 extension="${filename##*.}"
 name="${filename%.*}"
-pdflatex -output-directory=.output $1
+pdflatex --output-directory=.output $name
+openout_any=a bibtex .output/$name
+pdflatex --output-directory=.output $name
+pdflatex --output-directory=.output $name
 mv .output/$name.pdf .
 echo | evince $name.pdf &
